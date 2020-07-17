8db6f1277831d906ab2bd835b99ee0b9b44199e9
Bharathi Boda <50348970+bharathiboda@users.noreply.github.com>
2020-07-17 13:36:39 -0400

    Update lists.java

--- lists.java
+++ lists.java
@@ -51,3 +51,42 @@
             System.out.println(twoDlist.get(k).toString() + "\n");
         }
     }
+
+public static void main(String[] args) {
+        PracticeEmployee emp1 = new PracticeEmployee(1003, "java1", "Hyderabad");
+        PracticeEmployee emp2 = new PracticeEmployee(1002, "java2", "Pune");
+        PracticeEmployee emp3 = new PracticeEmployee(1001, "java3", "Delhi");
+        PracticeEmployee emp4 = new PracticeEmployee(1002, "java2", "Pune");
+
+        PracticeEmployee[] emps = {emp1, emp2, emp3, emp4};
+        List<PracticeEmployee> list1 = new ArrayList<>(Arrays.asList(emps));
+
+        // creating list2
+
+        PracticeEmployee emp5 = new PracticeEmployee(1009, "java4", "Benguluru");
+        PracticeEmployee emp6 = new PracticeEmployee(1007, "Java5", "Bombay");
+        PracticeEmployee emp7 = new PracticeEmployee(1008, "java6", "Chennai");
+
+        PracticeEmployee[] emps1 = {emp5, emp6, emp7};
+        List<PracticeEmployee> list2 = new ArrayList<>(Arrays.asList(emps1));
+
+        //creating list3
+        PracticeEmployee emp8 = new PracticeEmployee(1005, "java7", "Kolkata");
+        PracticeEmployee emp9 = new PracticeEmployee(1004, "Java8", "Amaravati");
+        PracticeEmployee emp10 = new PracticeEmployee(1006, "java9", "Chennai");
+
+        PracticeEmployee[] emps3 = {emp8, emp9, emp10};
+        List<PracticeEmployee> list3 = new ArrayList<>(Arrays.asList(emps3));
+
+
+        PracticeEmployee emp11 = new PracticeEmployee(1001, "Java12", "Hyderabad");
+        PracticeEmployee emp12 = new PracticeEmployee(1002, "Java11", "Pune");
+        PracticeEmployee emp13 = new PracticeEmployee(1007,  " Java10", "Chennai");
+
+        PracticeEmployee[] emps4 = {emp11, emp12, emp13};
+        List<PracticeEmployee> list4 = new ArrayList<>(Arrays.asList(emps4));
+        List<List<PracticeEmployee>> listOfList = new ArrayList<>();
+        listOfList.add(list1);
+        listOfList.add(list2);
+        listOfList.add(list3);
+        listOfList.add(list4);
